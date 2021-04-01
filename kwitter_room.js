9836be7deda35c2856be652046ef7df2ee404960
AATG-ui <76041889+AATG-ui@users.noreply.github.com>
2021-04-01 12:56:10 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDjYamnnTEW_L9B246wdYJ-XQi4BHiPoRo",
+    authDomain: "greetingbot-wxto.firebaseapp.com",
+    databaseURL: "https://greetingbot-wxto-default-rtdb.firebaseio.com",
+    projectId: "greetingbot-wxto",
+    storageBucket: "greetingbot-wxto.appspot.com",
+    messagingSenderId: "642206428328",
+    appId: "1:642206428328:web:9400760351ec19a104669d",
+    measurementId: "G-J1WF10RXGM"
+  };
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
